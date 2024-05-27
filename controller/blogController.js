5e558167c4caf9d52de924664b91eb1170a551a7
Harshit Vishwakarma <134127271+HarshitVishwakrma@users.noreply.github.com>
2024-05-27 15:12:48 +0530

    Update blogController.js

--- controller/blogController.js
+++ controller/blogController.js
@@ -221,6 +221,6 @@ exports.deleteComment = async (req, res) => {
 
     res.status(200).json({message :'Comment deleted successfully!'});
   } catch (err) {
-   res.status(500).json({error : error.message});
+   res.status(500).json({error : err.message});
   }
 };
